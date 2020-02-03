90a7d808e6b05d651c8192e236958630e8eb7f2a
Rasim <Rasim-2550@hotmail.com>
2020-02-04 01:58:17 +0300

    refactor ras table component

--- src/components/rasComp/rasTableComponent.vue
+++ src/components/rasComp/rasTableComponent.vue
@@ -11,6 +11,44 @@
                         single-line
                         hide-details
                 />
+                <v-dialog v-model="dialog" max-width="500px">
+                    <template v-slot:activator="{ on }">
+                        <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">{{ formTitle }}</span>
+                        </v-card-title>
+
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.name" label="Dessert name"/>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.calories" label="Calories"/>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.fat" label="Fat (g)"/>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.carbs" label="Carbs (g)"/>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.protein" label="Protein (g)"/>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-spacer/>
+                            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                            <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
             </v-card-title>
             <v-spacer/>
             <v-data-table
@@ -21,8 +59,30 @@
                     :search="search"
                     :loading="loading"
                     class="elevation-1"
-            />
+                    :items-per-page="5"
+            >
+
+            <template v-slot:item.action="{ item }">
+                <v-icon
+                        small
+                        class="mr-2"
+                        @click="editItem(item)"
+                >
+                    edit
+                </v-icon>
+                <v-icon
+                        small
+                        class="mr-2"
+                        @click="deleteItem(item)"
+                >
+                    delete
+                </v-icon>
+            </template>
+            </v-data-table>
         </v-card>
+        <br/>
+        <br/>
+        <br/>
     </v-app>
 </template>
 
@@ -43,7 +103,6 @@
             {
                 text: 'Dessert (100g serving)',
                 align: 'left',
-                sortable: false,
                 value: 'name',
             },
             {text: 'Calories', value: 'calories'},
@@ -51,7 +110,24 @@
             {text: 'Carbs (g)', value: 'carbs'},
             {text: 'Protein (g)', value: 'protein'},
             {text: 'Iron (%)', value: 'iron'},
+            {text: 'Actions', value: 'action', sortable: false},
         ];
+        dialog: boolean = false;
+        editedIndex: number = -1;
+        editedItem: RasTableModel = {
+            name: '',
+            calories: 0,
+            fat: 0,
+            carbs: 0,
+            protein: 0,
+        };
+        defaultItem: RasTableModel = {
+            name: '',
+            calories: 0,
+            fat: 0,
+            carbs: 0,
+            protein: 0,
+        };
 
         @Watch("search")
         @Watch("options", {deep: true, immediate: true})
@@ -67,6 +143,39 @@
                         this.totalItems = res.total;
                     }
                 })
+        }
+
+        get formTitle () {
+            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        }
+        // @Watch("dialog")
+        // dialog (val: boolean) {
+        //     val || this.close()
+        // }
+
+        editItem (item: RasTableModel) {
+            this.editedIndex = this.items.indexOf(item);
+            this.editedItem = Object.assign({}, item);
+            this.dialog = true
+        }
+        deleteItem (item: RasTableModel) {
+            const index = this.items.indexOf(item);
+            confirm('Are you sure you want to delete this item?') && this.items.splice(index, 1)
+        }
+        close () {
+            this.dialog = false;
+            setTimeout(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            }, 300)
+        }
+        save () {
+            if (this.editedIndex > -1) {
+                Object.assign(this.items[this.editedIndex], this.editedItem)
+            } else {
+                this.items.push(this.editedItem)
+            }
+            this.close()
         }
 
         getDataFromApi() {
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -7,4 +7,35 @@ export default new Vuetify({
     icons: {
         iconfont: 'mdi', // default - only for display purposes
     },
+    // complete: VuetifyIcon
+    // cancel: VuetifyIcon
+    // close: VuetifyIcon
+    // delete: VuetifyIcon
+    // clear: VuetifyIcon
+    // success: VuetifyIcon
+    // info: VuetifyIcon
+    // warning: VuetifyIcon
+    // error: VuetifyIcon
+    // prev: VuetifyIcon
+    // next: VuetifyIcon
+    // checkboxOn: VuetifyIcon
+    // checkboxOff: VuetifyIcon
+    // checkboxIndeterminate: VuetifyIcon
+    // delimiter: VuetifyIcon
+    // sort: VuetifyIcon
+    // expand: VuetifyIcon
+    // menu: VuetifyIcon
+    // subgroup: VuetifyIcon
+    // dropdown: VuetifyIcon
+    // radioOn: VuetifyIcon
+    // radioOff: VuetifyIcon
+    // edit: VuetifyIcon
+    // ratingEmpty: VuetifyIcon
+    // ratingFull: VuetifyIcon
+    // ratingHalf: VuetifyIcon
+    // loading: VuetifyIcon
+    // first: VuetifyIcon
+    // last: VuetifyIcon
+    // unfold: VuetifyIcon
+    //     [name: string]: VuetifyIcon
 });
